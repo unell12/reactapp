f72383280d04b2583be63331ea278d08f93809e6
Artemia <mute08@naver.com>
2022-03-02 20:28:42 +0900

    Fetch detail

--- reactproject/src/routes/Detail.js
+++ reactproject/src/routes/Detail.js
@@ -1,8 +1,30 @@
 import React from "react";
+import './Detail.css'
 
-function Detail(props){
-    console.log(props)
-    return <span>hello</span>
+class Detail extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+        if (location.state === undefined) {
+            history.push('/')
+        }
+    }
+
+    render() {
+        const { location } = this.props
+        if(location.state){
+            return <div className="box">
+                <div className="title">
+                    <em>{location.state.title}</em>
+                </div>
+                <div className="year">
+                    {location.state.year}
+                </div>
+                <img src={location.state.poster} alt={location.state.title} title={location.state.title} />
+            </div>
+        } else {
+            return null;
+        }
+    }
 }
 
 export default Detail;
